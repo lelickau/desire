30e9b0aa0cda2d6fcc54a8130d23a9d6b93bb084
elena_kau <lelickau@yandex.ru>
2021-05-14 15:02:11 +0200

    14.05.21

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,13 @@
 $(function () {
     $('.header__btn').on('click', function () {
-        $('.rightside-menu').removeClass('rightside-menu--close')
-    })
+        $('.rightside-menu').removeClass('rightside-menu--close');
+    });
     $('.rightside-menu__close').on('click', function () {
-        $('.rightside-menu').addClass('rightside-menu--close')
-    })
+        $('.rightside-menu').addClass('rightside-menu--close');
+    });
+    $('.top__slider').slick({
+        dots: true,
+        arrows: false,
+        autoplay: true
+    });
 })
