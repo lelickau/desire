e94c54f3d9a79cbac4bebd83aa86d5a71f65830f
elena_kau <lelickau@yandex.ru>
2021-08-16 16:37:19 +0200

    adaptive 320px

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### [Desire](lelickau.github.io/desire/dist/index.html) Многостраничный сайт (6 страниц).
+### [Desire](lelickau.github.io/desire/dist/index.html) Многостраничный сайт (6 страниц). HTML/CSS(SCSS)/JavaScript (jquery)
 
 ### Gulp сборка:
 - [x] browser-sync - отслеживание изменений в исходных файлах и скриптах без перезагрузки веб-страницы.
@@ -9,3 +9,11 @@
 - [x] gulp-sass - препроцессор.
 - [x] gulp-uglify-es - минификация.
 - [x] jquery
+
+#### Слайдеры: использован [SlickSlider](https://kenwheeler.github.io/slick/)
+
+#### Галереи картинок на grid
+
+#### Фильтр картинок галереи: использован [MixItUp](https://www.kunkalabs.com/mixitup/)
+
+#### Видео pop-up: [Fancybox](https://fancyapps.com)
--- app/js/main.js
+++ app/js/main.js
@@ -9,7 +9,7 @@ $(function () {
     $('.header__btn-menu').on('click', function () {
         $('.menu').toggleClass('menu--open');
     });
-    
+
     $('.top__slider').slick({
         dots: true,
         arrows: false,
@@ -21,6 +21,53 @@ $(function () {
         slidesToScroll: 10,
         dots: true,
         arrows: false,
+        responsive: [{
+                breakpoint: 1700,
+                settings: {
+                    slidesToShow: 8,
+                    slidesToScroll: 8,
+                }
+            },
+            {
+                breakpoint: 1511,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 6,
+                }
+            },
+            {
+                breakpoint: 1201,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                }
+            },
+            {
+                breakpoint: 841,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 551,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: true,
+                    dots: false,
+                    prevArrow: '<button type="button" class="slick-prev"><img src="./images/arrow-left-d.svg" alt="prev"></button>',
+                    nextArrow: '<button type="button" class="slick-next"><img src="./images/arrow-right-d.svg" alt="next"></button>',
+                }
+            }
+        ]
     });
     $('.article-slider__box').slick({
         prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/blog/arr-slide-left.svg" alt="left"></button>',
@@ -33,7 +80,7 @@ $(function () {
         }
     });
 
-    if($(window).width() < 651) {
+    if ($(window).width() < 651) {
         $('.works-path__item--measuring').appendTo($('.works-path__items-box'));
     }
 })
